40c0b8fc2260c9206e67f38a9d810d7c2bdcaf47
johnston9 <davejohnston9@gmail.com>
2021-12-01 17:06:11 +0000

    Comments

---
+++ src/pages/comments/Comment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import styles from "../../styles/Comment.module.css";
+
+const Comment = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Comment
