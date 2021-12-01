a0279faa8b5e37b0ff2a143757be908d2374b59e
johnston9 <davejohnston9@gmail.com>
2021-12-01 20:49:20 +0000

    Edit comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 import Avatar from "../../components/Avatar";
+import { PostDropdown } from "../../components/PostDropdown";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import CommentEditForm from "./CommentEditForm";
 import styles from "../../styles/Comment.module.css";
 
 const Comment = (props) => {
+    const { profile_id, profile_image, owner, updated_at, 
+        content, id, setPost, setComments, } = props;
+    
+    const [showEditForm, setShowEditForm] = useState(false);
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+          await axiosRes.delete(`/comments/${id}/`);
+          setPost((prevPost) => ({
+            results: [
+              {
+                ...prevPost.results[0],
+                comments_count: prevPost.results[0].comments_count - 1,
+              },
+            ],
+          }));
+    
+          setComments((prevComments) => ({
+            ...prevComments,
+            results: prevComments.results.filter((comment) => comment.id !== id),
+          }));
+        } catch (err) {}
+      };
+
     return (
         <div>
-            
+            <hr />
+            <div className="media">
+                <Link to={`/profiles/${profile_id}`} >
+                    <Avatar src={profile_image} />
+                </Link>
+                <div className="media-body align-self-center ml-1">
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    {showEditForm ? (
+                        <CommentEditForm
+                            id={id}
+                            profile_id={profile_id}
+                            content={content}
+                            profileImage={profile_image}
+                            setComments={setComments}
+                            setShowEditForm={setShowEditForm}
+                        />
+                    ) : (
+                        <p>{content}</p>
+                    )}
+                </div>
+                {is_owner && !showEditForm && (
+                    <PostDropdown
+                        handleEdit={() => setShowEditForm(true)}
+                        handleDelete={handleDelete}
+                    />
+                    )}
+            </div>  
         </div>
     )
 }
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,6 +11,9 @@ import Post from "./Post";
 import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
     const { id } = useParams()
@@ -57,9 +60,19 @@ function PostPage() {
         ) : null}
         {/* comments */}
         {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
-            ))
+          <InfiniteScroll
+            children={comments.results.map((comment) => (
+              <Comment key={comment.id} 
+              {...comment} 
+              setPost={setPost}
+              setComments={setComments}/>
+            ))}
+            dataLength={comments.results.length}
+            loader={<Asset spinner /> }
+            hasMore={!!comments.next}
+            next={() => fetchMoreData(comments, setComments)}
+            />
+            
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
