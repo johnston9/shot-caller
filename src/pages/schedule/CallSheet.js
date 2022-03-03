1d572b646c6901ef0a6ee66d5092176ff1926de1
johnston9 <davejohnston9@gmail.com>
2022-03-03 17:39:21 +0000

    Callsheet

--- src/pages/schedule/CallSheet.js
+++ src/pages/schedule/CallSheet.js
@@ -59,6 +59,158 @@ const CallSheet = () => {
         >
             Back
         </Button>
+        {/* top */}
+        <Row>
+            <Col>
+            <div>
+            <h5>Production Company</h5>
+            <h5>Production Company Address</h5>
+            <h5>Production Company Phone</h5>
+            <h5>Production Company email</h5>
+            </div>
+            <div>
+            <h5>Producer</h5>
+            <h5>Coordinator</h5>
+            <h5>Director</h5>
+            <h5>1st AD</h5>
+            <h5>DoP</h5>
+            </div>
+            </Col>
+            <Col>
+            <div>
+            <h5>Production Name</h5>
+            <h5>Production Company Logo</h5>
+            </div>
+            <div>
+            <h3>Unit Call Time</h3>
+            <h5>First Location Address</h5>
+            </div>
+            </Col>
+            <Col>
+            <div>
+            <h5>Weather</h5>
+            <h5>Sunrise</h5>
+            <h5>Sunset</h5>
+            </div>
+            <div>
+            <h5>Nearest Hospital</h5>
+            <h5>Hospital Address</h5>
+            <h5>Sunset</h5>
+            </div>
+            <div>
+            <h5>Talent Call</h5>
+            <h5>Furst Shot</h5>
+            <h5>Lunch</h5>
+            <h5>Est. Wrap</h5>
+            </div>
+            </Col>
+        </Row>
+        {/* Locations */}
+        <h4>Locations</h4>
+        {/* Titles */}
+        <Row>
+            <Col>
+            <h5>Basecamp</h5>
+            </Col>
+            <Col>
+            <h5>Crew Park</h5>
+            </Col>
+            <Col>
+            <h5>Teck Trucks</h5>
+            </Col>
+            <Col>
+            <h5>BG holding</h5>
+            </Col>
+            <Col>
+            <h5>BG Parking</h5>
+            </Col>
+            <Col>
+            <h5>Parking and Notes Name</h5>
+            </Col>
+            <Col>
+            <h5>Nearest Hostipal</h5>
+            </Col>
+            <Col>
+            <h5>Google Maps</h5>
+            </Col>
+        </Row>
+        {/* Locations */}
+        <h4>Locations</h4>
+        {/* Titles */}
+        <Row>
+            <Col>
+            <h5>Number</h5>
+            </Col>
+            <Col>
+            <h5>Location Name</h5>
+            </Col>
+            <Col>
+            <h5>Location Address</h5>
+            </Col>
+            <Col>
+            <h5>Parking and Notes Name</h5>
+            </Col>
+            <Col>
+            <h5>Nearest Hostipal</h5>
+            </Col>
+            <Col>
+            <h5>Google Maps</h5>
+            </Col>
+        </Row>
+        {/* actual */}
+        <Row>
+            <Col>
+            <h5>Number</h5>
+            </Col>
+            <Col>
+            <h5>Location Name</h5>
+            </Col>
+            <Col>
+            <h5>Location Address</h5>
+            </Col>
+            <Col>
+            <h5>Parking and Notes Name</h5>
+            </Col>
+            <Col>
+            <h5>Nearest Hostipal</h5>
+            </Col>
+            <Col>
+            <h5>Google Maps</h5>
+            </Col>
+        </Row>
+        {/* Crew Contacts */}
+        <Row>
+            <Col>
+            <h5>Role</h5>
+            </Col>
+            <Col>
+            <h5>Name</h5>
+            </Col>
+            <Col>
+            <h5>Contact Info</h5>
+            </Col>
+            <Col>
+            <h5>Call Time</h5>
+            </Col>
+        </Row>
+        {/* Talent */}
+        <Row>
+            <Col>
+            <h5>Role</h5>
+            </Col>
+            <Col>
+            <h5>Name</h5>
+            </Col>
+            <Col>
+            <h5>Contact Info</h5>
+            </Col>
+            <Col>
+            <h5>Call Time</h5>
+            </Col>
+            <Col>
+            <h5>Pickup</h5>
+            </Col>
+        </Row>
     </div>
   )
 }
