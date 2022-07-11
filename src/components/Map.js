93f7742e0d4b5cb0e2937a7584c792b9a82e054f
johnston9 <davejohnston9@gmail.com>
2022-07-11 11:42:04 +0000

    add map marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,28 @@
 import React, { useMemo } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import styles from "../styles/Map.module.css";
 // import TopBox from '../../components/TopBox';
 import { GoogleMap, Marker, useJsApiLoader, useLoadScript } from '@react-google-maps/api';
 
 function MapPage() {
   // const center = useMemo(() => ({lat: 53.3498, lng: 6.2603 }), []) ;
-  // center={{lat: 53.3498, lng: 6.2603 }}
+  // center={{lat: 53.3498, lng: -6.2603 }}
+  // position={{lat: 40.7128, lng: -74.0060 }}
   return (
     <div>
+    <Row className="mt-3">
+    <Col md={{span: 10, offset:1 }}>
     <GoogleMap 
     zoom={10}
-    center={{lat: 40.7128, lng: -74.0060 }}
+    center={{lat: 53.3498, lng: -6.2603 }}
     mapContainerClassName={` ${styles.Map}`}
-     ></GoogleMap>
-     <Marker 
-     position={{lat: 40.7128, lng: -74.0060 }} />
+     >
+      <Marker 
+     position={{lat: 53.3498, lng: -6.2603 }} />
+     </GoogleMap>
+    </Col>
+    </Row>
     </div>
   )
 }
